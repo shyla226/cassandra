5bf1e47902b95b3d9dc0087e2ec19b23f43a4739
Tyler Hobbs <tylerlhobbs@gmail.com>
2016-09-29 17:31:27 -0500

    Revert "Don't go through scheduler for local reads"
    This reverts commit b7d4d0a784d29e8f0fc3f2549d27e22ae17e4070.

--- src/java/org/apache/cassandra/service/AbstractReadExecutor.java
+++ src/java/org/apache/cassandra/service/AbstractReadExecutor.java
@@ -23,19 +23,18 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.collect.Iterables;
-import org.apache.cassandra.config.DatabaseDescriptor;
-import org.apache.cassandra.db.*;
-import org.apache.cassandra.db.filter.TombstoneOverwhelmingException;
-import org.apache.cassandra.db.monitoring.ConstructionTime;
-import org.apache.cassandra.db.partitions.UnfilteredPartitionIterator;
-import org.apache.cassandra.exceptions.RequestFailureReason;
-import org.apache.cassandra.utils.FBUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.reactivex.Observable;
 import org.apache.cassandra.concurrent.NettyRxScheduler;
 import org.apache.cassandra.config.ReadRepairDecision;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.ConsistencyLevel;
+import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.Keyspace;
+import org.apache.cassandra.db.ReadCommand;
+import org.apache.cassandra.db.SinglePartitionReadCommand;
 import org.apache.cassandra.db.partitions.PartitionIterator;
 import org.apache.cassandra.exceptions.ReadFailureException;
 import org.apache.cassandra.exceptions.ReadTimeoutException;
@@ -115,48 +114,16 @@ public abstract class AbstractReadExecutor
         if (hasLocalEndpoint)
         {
             logger.trace("reading {} locally", readCommand.isDigestQuery() ? "digest" : "data");
-            executeLocalRead();
-        }
-    }
 
-    private void executeLocalRead()
-    {
-        long start = System.nanoTime();
-        long constructionTime = System.currentTimeMillis();
-        try
-        {
-            command.setMonitoringTime(new ConstructionTime(constructionTime), MessagingService.Verb.READ.getTimeout(), DatabaseDescriptor.getSlowQueryTimeout());
-
-            ReadResponse response;
-            try (ReadExecutionController executionController = command.executionController();
-                 UnfilteredPartitionIterator iterator = command.executeLocally(executionController))
+            if (command instanceof SinglePartitionReadCommand)
             {
-                response = command.createResponse(iterator);
-            }
-
-            if (command.complete())
-            {
-                handler.response(response);
+                SinglePartitionReadCommand singleCommand = (SinglePartitionReadCommand) command;
+                NettyRxScheduler.getForKey(Keyspace.openAndGetStore(command.metadata()), singleCommand.partitionKey())
+                    .scheduleDirect(() -> new StorageProxy.LocalReadRunnable(command, handler).runMayThrow());
             }
             else
             {
-                MessagingService.instance().incrementDroppedMessages(MessagingService.Verb.READ, System.currentTimeMillis() - constructionTime);
-                handler.onFailure(FBUtilities.getBroadcastAddress(), RequestFailureReason.UNKNOWN);
-            }
-
-            MessagingService.instance().addLatency(FBUtilities.getBroadcastAddress(), TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start));
-        }
-        catch (Throwable t)
-        {
-            if (t instanceof TombstoneOverwhelmingException)
-            {
-                handler.onFailure(FBUtilities.getBroadcastAddress(), RequestFailureReason.READ_TOO_MANY_TOMBSTONES);
-                logger.error(t.getMessage());
-            }
-            else
-            {
-                handler.onFailure(FBUtilities.getBroadcastAddress(), RequestFailureReason.UNKNOWN);
-                throw t;
+                NettyRxScheduler.instance().scheduleDirect(() -> new StorageProxy.LocalReadRunnable(command, handler).runMayThrow());
             }
         }
     }
@@ -322,6 +289,8 @@ public abstract class AbstractReadExecutor
             // no latency information, or we're overloaded
             if (cfs.sampleLatencyNanos > TimeUnit.MILLISECONDS.toNanos(command.getTimeout()))
                 return;
+
+            Observable<PartitionIterator> observable = get();
 
             NettyRxScheduler.instance().scheduleDirect(() -> {
 
