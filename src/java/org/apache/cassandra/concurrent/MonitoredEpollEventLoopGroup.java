22a0b3faae42fe37d31e2620c014a47d8da9dd10
T Jake Luciani <jake@apache.org>
2016-10-14 15:40:30 -0400

    larger queue

--- src/java/org/apache/cassandra/concurrent/MonitoredEpollEventLoopGroup.java
+++ src/java/org/apache/cassandra/concurrent/MonitoredEpollEventLoopGroup.java
@@ -193,12 +193,12 @@ public class MonitoredEpollEventLoopGroup extends MultithreadEventLoopGroup
 
             this.threadOffset = threadOffset;
 
-            this.externalQueue = new MpscArrayQueue<>(1 << 15);
+            this.externalQueue = new MpscArrayQueue<>(1 << 16);
 
             this.incomingQueues = new MessagePassingQueue[totalCores];
             for (int i = 0; i < incomingQueues.length; i++)
             {
-                incomingQueues[i] = new SpscArrayQueue<>(1 << 15);
+                incomingQueues[i] = new SpscArrayQueue<>(1 << 16);
             }
 
             this.state = CoreState.WORKING;
