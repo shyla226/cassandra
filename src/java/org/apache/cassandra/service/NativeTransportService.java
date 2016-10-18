740fab49b4f87b05d409dbbc31c693d3c2003449
T Jake Luciani <jake@apache.org>
2016-10-18 15:54:31 -0400

    affinity

--- src/java/org/apache/cassandra/service/NativeTransportService.java
+++ src/java/org/apache/cassandra/service/NativeTransportService.java
@@ -54,9 +54,9 @@ public class NativeTransportService
     private List<EventLoopGroup> workerGroups = Collections.emptyList();
 
     private static Integer pIO = Integer.valueOf(System.getProperty("io.netty.ratioIO", "50"));
-    private static Boolean affinity = Boolean.valueOf(System.getProperty("io.netty.affinity","false"));
+    private static Boolean affinity = Boolean.valueOf(System.getProperty("io.netty.affinity","true"));
 
-    public static final int NUM_NETTY_THREADS = Integer.valueOf(System.getProperty("io.netty.eventLoopThreads", String.valueOf(FBUtilities.getAvailableProcessors() * 1)));
+    public static final int NUM_NETTY_THREADS = Integer.valueOf(System.getProperty("io.netty.eventLoopThreads", String.valueOf(FBUtilities.getAvailableProcessors() - 1)));
 
     private boolean initialized = false;
     private boolean tpcInitialized = false;
