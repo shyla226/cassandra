51f468ff3226d08607a4328930417ea7611a28e3
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2017-01-25 17:37:23 +0800

    Port of apollo-265, use DecayingEstimatedHistogramReservoir in DynamicEndpointSnitch

--- src/java/org/apache/cassandra/locator/DynamicEndpointSnitch.java
+++ src/java/org/apache/cassandra/locator/DynamicEndpointSnitch.java
@@ -26,7 +26,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import com.codahale.metrics.ExponentiallyDecayingReservoir;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
@@ -36,6 +35,7 @@ import org.apache.cassandra.gms.ApplicationState;
 import org.apache.cassandra.gms.EndpointState;
 import org.apache.cassandra.gms.Gossiper;
 import org.apache.cassandra.gms.VersionedValue;
+import org.apache.cassandra.metrics.DecayingEstimatedHistogramReservoir;
 import org.apache.cassandra.net.MessagingService;
 import org.apache.cassandra.service.StorageService;
 import org.apache.cassandra.utils.FBUtilities;
@@ -48,8 +48,8 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
 {
     private static final boolean USE_SEVERITY = !Boolean.getBoolean("cassandra.ignore_dynamic_snitch_severity");
 
-    private static final double ALPHA = 0.75; // set to 0.75 to make EDS more biased to towards the newer values
-    private static final int WINDOW_SIZE = 100;
+    // legacy value corresponding to a window size of 100 when using com.codahale.metrics.ExponentiallyDecayingReservoi
+    static final long MAX_LATENCY = 107964792;
 
     private volatile int dynamicUpdateInterval = DatabaseDescriptor.getDynamicUpdateInterval();
     private volatile int dynamicResetInterval = DatabaseDescriptor.getDynamicResetInterval();
@@ -62,16 +62,15 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
     private String mbeanName;
     private boolean registered = false;
 
-    private volatile HashMap<InetAddress, Double> scores = new HashMap<>();
-    private final ConcurrentHashMap<InetAddress, ExponentiallyDecayingReservoir> samples = new ConcurrentHashMap<>();
+    private volatile Map<InetAddress, Double> scores = Collections.emptyMap();
+    private final ConcurrentHashMap<InetAddress, DecayingEstimatedHistogramReservoir> samples = new ConcurrentHashMap<>();
 
     public final IEndpointSnitch subsnitch;
 
     private volatile ScheduledFuture<?> updateSchedular;
     private volatile ScheduledFuture<?> resetSchedular;
 
-    private final Runnable update;
-    private final Runnable reset;
+    private static final double percentile = .9;
 
     public DynamicEndpointSnitch(IEndpointSnitch snitch)
     {
@@ -84,27 +83,11 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
         if (instance != null)
             mbeanName += ",instance=" + instance;
         subsnitch = snitch;
-        update = new Runnable()
-        {
-            public void run()
-            {
-                updateScores();
-            }
-        };
-        reset = new Runnable()
-        {
-            public void run()
-            {
-                // we do this so that a host considered bad has a chance to recover, otherwise would we never try
-                // to read from it, which would cause its score to never change
-                reset();
-            }
-        };
 
         if (DatabaseDescriptor.isDaemonInitialized())
         {
-            updateSchedular = ScheduledExecutors.scheduledTasks.scheduleWithFixedDelay(update, dynamicUpdateInterval, dynamicUpdateInterval, TimeUnit.MILLISECONDS);
-            resetSchedular = ScheduledExecutors.scheduledTasks.scheduleWithFixedDelay(reset, dynamicResetInterval, dynamicResetInterval, TimeUnit.MILLISECONDS);
+            updateSchedular = ScheduledExecutors.scheduledTasks.scheduleWithFixedDelay(this::updateScores, dynamicUpdateInterval, dynamicUpdateInterval, TimeUnit.MILLISECONDS);
+            resetSchedular = ScheduledExecutors.scheduledTasks.scheduleWithFixedDelay(this::reset, dynamicResetInterval, dynamicResetInterval, TimeUnit.MILLISECONDS);
             registerMBean();
         }
     }
@@ -121,7 +104,7 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
             if (DatabaseDescriptor.isDaemonInitialized())
             {
                 updateSchedular.cancel(false);
-                updateSchedular = ScheduledExecutors.scheduledTasks.scheduleWithFixedDelay(update, dynamicUpdateInterval, dynamicUpdateInterval, TimeUnit.MILLISECONDS);
+                updateSchedular = ScheduledExecutors.scheduledTasks.scheduleWithFixedDelay(this::updateScores, dynamicUpdateInterval, dynamicUpdateInterval, TimeUnit.MILLISECONDS);
             }
         }
 
@@ -131,7 +114,7 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
             if (DatabaseDescriptor.isDaemonInitialized())
             {
                 resetSchedular.cancel(false);
-                resetSchedular = ScheduledExecutors.scheduledTasks.scheduleWithFixedDelay(reset, dynamicResetInterval, dynamicResetInterval, TimeUnit.MILLISECONDS);
+                resetSchedular = ScheduledExecutors.scheduledTasks.scheduleWithFixedDelay(this::reset, dynamicResetInterval, dynamicResetInterval, TimeUnit.MILLISECONDS);
             }
         }
 
@@ -185,7 +168,7 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
 
     public List<InetAddress> getSortedListByProximity(final InetAddress address, Collection<InetAddress> addresses)
     {
-        List<InetAddress> list = new ArrayList<InetAddress>(addresses);
+        List<InetAddress> list = new ArrayList<>(addresses);
         sortByProximity(address, list);
         return list;
     }
@@ -210,14 +193,8 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
         // its comparator to be "stable", that is 2 endpoint should compare the same way for the duration
         // of the sort() call. As we copy the scores map on write, it is thus enough to alias the current
         // version of it during this call.
-        final HashMap<InetAddress, Double> scores = this.scores;
-        Collections.sort(addresses, new Comparator<InetAddress>()
-        {
-            public int compare(InetAddress a1, InetAddress a2)
-            {
-                return compareEndpoints(address, a1, a2, scores);
-            }
-        });
+        Map<InetAddress, Double> scores = this.scores;
+        addresses.sort((a1, a2) -> compareEndpoints(address, a1, a2, scores));
     }
 
     private void sortByProximityWithBadness(final InetAddress address, List<InetAddress> addresses)
@@ -226,8 +203,8 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
             return;
 
         subsnitch.sortByProximity(address, addresses);
-        HashMap<InetAddress, Double> scores = this.scores; // Make sure the score don't change in the middle of the loop below
-                                                           // (which wouldn't really matter here but its cleaner that way).
+        Map<InetAddress, Double> scores = this.scores; // Make sure the score don't change in the middle of the loop below
+                                                       // (which wouldn't really matter here but its cleaner that way).
         ArrayList<Double> subsnitchOrderedScores = new ArrayList<>(addresses.size());
         for (InetAddress inet : addresses)
         {
@@ -257,25 +234,12 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
     // Compare endpoints given an immutable snapshot of the scores
     private int compareEndpoints(InetAddress target, InetAddress a1, InetAddress a2, Map<InetAddress, Double> scores)
     {
-        Double scored1 = scores.get(a1);
-        Double scored2 = scores.get(a2);
-        
-        if (scored1 == null)
-        {
-            scored1 = 0.0;
-        }
+        double scored1 = scores.getOrDefault(a1, 0.0d);
+        double scored2 = scores.getOrDefault(a2, 0.0d);
 
-        if (scored2 == null)
-        {
-            scored2 = 0.0;
-        }
+        int cmp = Double.compare(scored1, scored2);
 
-        if (scored1.equals(scored2))
-            return subsnitch.compareEndpoints(target, a1, a2);
-        if (scored1 < scored2)
-            return -1;
-        else
-            return 1;
+        return cmp != 0 ? cmp : subsnitch.compareEndpoints(target, a1, a2);
     }
 
     public int compareEndpoints(InetAddress target, InetAddress a1, InetAddress a2)
@@ -288,14 +252,15 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
 
     public void receiveTiming(InetAddress host, long latency) // this is cheap
     {
-        ExponentiallyDecayingReservoir sample = samples.get(host);
+        // prevent the histogram from overflowing
+        if (MAX_LATENCY < latency)
+            latency = MAX_LATENCY;
+
+        DecayingEstimatedHistogramReservoir sample = samples.get(host);
         if (sample == null)
-        {
-            ExponentiallyDecayingReservoir maybeNewSample = new ExponentiallyDecayingReservoir(WINDOW_SIZE, ALPHA);
-            sample = samples.putIfAbsent(host, maybeNewSample);
-            if (sample == null)
-                sample = maybeNewSample;
-        }
+            // using samples.get() before the computeIfAbsent gives a slight perf improvement
+            sample = samples.computeIfAbsent(host, (h) -> new DecayingEstimatedHistogramReservoir(true, MAX_LATENCY));
+
         sample.update(latency);
     }
 
@@ -316,16 +281,16 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
         // We're going to weight the latency for each host against the worst one we see, to
         // arrive at sort of a 'badness percentage' for them. First, find the worst for each:
         HashMap<InetAddress, Double> newScores = new HashMap<>();
-        for (Map.Entry<InetAddress, ExponentiallyDecayingReservoir> entry : samples.entrySet())
+        for (Map.Entry<InetAddress, DecayingEstimatedHistogramReservoir> entry : samples.entrySet())
         {
-            double mean = entry.getValue().getSnapshot().getMedian();
+            double mean = entry.getValue().getSnapshot().getValue(percentile);
             if (mean > maxLatency)
                 maxLatency = mean;
         }
         // now make another pass to do the weighting based on the maximums we found before
-        for (Map.Entry<InetAddress, ExponentiallyDecayingReservoir> entry: samples.entrySet())
+        for (Map.Entry<InetAddress, DecayingEstimatedHistogramReservoir> entry: samples.entrySet())
         {
-            double score = entry.getValue().getSnapshot().getMedian() / maxLatency;
+            double score = entry.getValue().getSnapshot().getValue(percentile) / maxLatency;
             // finally, add the severity without any weighting, since hosts scale this relative to their own load and the size of the task causing the severity.
             // "Severity" is basically a measure of compaction activity (CASSANDRA-3722).
             if (USE_SEVERITY)
@@ -338,6 +303,8 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
 
     private void reset()
     {
+        // we do this so that a host considered bad has a chance to recover, otherwise would we never try
+        // to read from it, which would cause its score to never change
        samples.clear();
     }
 
@@ -366,13 +333,24 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
 
     public List<Double> dumpTimings(String hostname) throws UnknownHostException
     {
-        InetAddress host = InetAddress.getByName(hostname);
-        ArrayList<Double> timings = new ArrayList<Double>();
-        ExponentiallyDecayingReservoir sample = samples.get(host);
+        return dumpTimings(InetAddress.getByName(hostname));
+    }
+
+    public List<Double> dumpTimings(InetAddress host) throws UnknownHostException
+    {
+        ArrayList<Double> timings = new ArrayList<>();
+        DecayingEstimatedHistogramReservoir sample = samples.get(host);
         if (sample != null)
         {
-            for (double time: sample.getSnapshot().getValues())
-                timings.add(time);
+            long[] values = sample.getSnapshot().getValues();
+            long[] bucketOffsets = sample.getBucketOffsets();
+            for (int i = 0; i < values.length; i++)
+            {
+                for (long l = 0; l < values[i]; l++)
+                {
+                    timings.add((double) bucketOffsets[i]);
+                }
+            }
         }
         return timings;
     }
