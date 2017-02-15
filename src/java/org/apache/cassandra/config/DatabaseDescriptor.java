cea21a47f285aaee970b2f681d202f36227a6509
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2017-02-15 09:07:06 +0000

    Change some continuous paging config values
    patch by Stefania Alborghetti; reviewed by Benjamin Lerer for APOLLO-432

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1482,7 +1482,7 @@ public class DatabaseDescriptor
 
     public static int getContinuousPagingThreads()
     {
-        return conf.continuous_paging.getNumThreads();
+        return conf.continuous_paging.max_threads;
     }
 
     public static int getConcurrentWriters()
