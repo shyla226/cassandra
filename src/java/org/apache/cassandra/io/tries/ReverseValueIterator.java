f29b8bfb655c0b1c0dd0636947f0599823709540
Branimir Lambov <branimir.lambov@datastax.com>
2017-07-01 12:15:21 +0300

    Properly fix ReversedValueIterator.

--- src/java/org/apache/cassandra/io/tries/ReverseValueIterator.java
+++ src/java/org/apache/cassandra/io/tries/ReverseValueIterator.java
@@ -119,16 +119,16 @@ public class ReverseValueIterator<Concrete extends ReverseValueIterator<Concrete
         // This method must be async-read-safe. Every read from new buffering position (the go() calls) can
         // trigger NotInCacheException, and iteration must be able to redo the work that was interrupted during the next
         // call. Hence the mutable state must be fully ready before all go() calls
-        // (i.e. they must be the last step in the loop).
+        // (i.e. they must either be the last step in the loop or the state must be unchaged until that call has succeeded).
         go(stack.node);
         while (true)
         {
             // advance position in node
-            --stack.childIndex;
+            int childIdx = stack.childIndex - 1;
             boolean beyondLimit = true;
-            if (stack.childIndex >= 0)
+            if (childIdx >= 0)
             {
-                transitionByte = transitionByte(stack.childIndex);
+                transitionByte = transitionByte(childIdx);
                 beyondLimit = transitionByte < stack.limit;
                 if (beyondLimit)
                 {
@@ -160,15 +160,21 @@ public class ReverseValueIterator<Concrete extends ReverseValueIterator<Concrete
                 continue;
             }
 
-            child = transition(stack.childIndex);
+            child = transition(childIdx);
             if (child != -1)
             {
+                // This call could throw NotInCacheException. If so, the loop will be re-entered in the same state as
+                // it was before entering this loop.
+                go(child);
+
+                stack.childIndex = childIdx;
+
                 // descend, stack up position
                 int l = -1;
                 if (transitionByte == stack.limit)
                     l = limit.next();
+
                 stack = new IterationPosition(child, transitionRange(), l, stack);
-                go(child);
             }
         }
     }
