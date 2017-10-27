863982dbe20c5c4157762d81755e03b23f38560d
Benjamin Lerer <b.lerer@gmail.com>
2017-10-27 17:15:46 +0200

    Avoid loading unecessary dependencies

--- src/java/com/datastax/bdp/db/audit/CassandraAuditWriterOptions.java
+++ src/java/com/datastax/bdp/db/audit/CassandraAuditWriterOptions.java
@@ -5,8 +5,6 @@
  */
 package com.datastax.bdp.db.audit;
 
-import org.apache.cassandra.db.ConsistencyLevel;
-
 public class CassandraAuditWriterOptions
 {
     public String mode = "sync";
@@ -15,6 +13,6 @@ public class CassandraAuditWriterOptions
     public String flush_time = "500";
     public String queue_size = "10000";
     public String day_partition_millis = Integer.valueOf(1000 * 60 * 60).toString();  // partition days into hours by default
-    public String write_consistency = ConsistencyLevel.QUORUM.toString();
+    public String write_consistency = "QUORUM";
     public String dropped_event_log = "/var/log/cassandra/dropped_audit_events.log";
 }
