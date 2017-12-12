4fc069373670548a49a65353fd639d6d7d2c57ca
nitsanw <nitsanw@yahoo.com>
2017-12-12 23:50:12 +0200

    Replace Cell[] with a primitive array to reduce indirection

--- src/java/org/apache/cassandra/utils/concurrent/LongAdder.java
+++ src/java/org/apache/cassandra/utils/concurrent/LongAdder.java
@@ -1,28 +1,14 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright DataStax, Inc.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Please see the included license file for details.
  */
 
 package org.apache.cassandra.utils.concurrent;
 
-import java.util.Arrays;
-
 import org.apache.cassandra.concurrent.TPC;
-import org.apache.cassandra.utils.UnsafeAccess;
-import sun.misc.Contended;
+
+import static org.apache.cassandra.utils.UnsafeAccess.UNSAFE;
 
 /**
  * A class equivalent to {@link java.util.concurrent.atomic.LongAdder} except that
@@ -37,14 +23,18 @@ import sun.misc.Contended;
  * */
 public class LongAdder
 {
-    private static final long valueOffset;
+    // to prevent false sharing we need to pad by 2 cache lines, typically 64b per cache line.
+    private static final int FALSE_SHARING_PAD = 128;
+    private static final int COUNTER_OFFSET_SCALE = Integer.numberOfTrailingZeros(FALSE_SHARING_PAD);
+    private static final int SIZEOF_LONG = 8;
+    private static final long COUNTER_ARRAY_BASE;
 
     static
     {
         try
         {
-            Class<?> ak = Cell.class;
-            valueOffset = UnsafeAccess.UNSAFE.objectFieldOffset(ak.getDeclaredField("value"));
+            // first counter offset in array, padded to the 'left'
+            COUNTER_ARRAY_BASE = UNSAFE.arrayBaseOffset(long[].class) + FALSE_SHARING_PAD;
         }
         catch (Exception e)
         {
@@ -52,69 +42,38 @@ public class LongAdder
         }
     }
 
-    static final class Cell {
-        @Contended
-        long value;
-
-        Cell(long x) { value = x; }
-
-        /** Lazyness does not guarantee the value that is written
-         * will be immediately visible to another thread but it does
-         * ensure that the update will be atomic (i.e. either visible
-         * or not at all) and typically a reading thread will have access
-         * in a few nanoseconds. It is suitable when only a single thread
-         * updates a value, even for incrementing, since we know the current
-         * value in this case.
-         */
-        void add(long x, boolean lazy)
-        {
-            if (lazy)
-                UnsafeAccess.UNSAFE.putOrderedLong(this, valueOffset, value + x);
-            else
-                UnsafeAccess.UNSAFE.getAndAddLong(this, valueOffset, x);
-        }
-
-        void set(long x)
-        {
-            UnsafeAccess.UNSAFE.getAndSetLong(this, valueOffset, x);
-        }
-
-        long get()
-        {
-            return UnsafeAccess.UNSAFE.getLongVolatile(this, valueOffset);
-        }
-    }
-
     private final int numCores;
-    private final Cell[] values;
+    private final long[] values;
 
     public LongAdder()
     {
         this.numCores = TPC.getNumCores();
-        this.values = new Cell[numCores + 1];
-
-        // The last value is shared by multiple threads and so it is
-        // initialized early to avoid races during lazy initialization
-        this.values[numCores] = new Cell(0L);
+        // each counter is padded to the right by 2 cache lines(128b) and we need one extra pad on the left
+        this.values = new long[(numCores + 2)*(FALSE_SHARING_PAD / SIZEOF_LONG)];
     }
 
     public void add(long x)
     {
-       int coreId = TPC.getCoreId();
+        int coreId = TPC.getCoreId();
 
-        // allocate lazily to conserve space and to make sure they
-        // are not on the same cache line, even with the @contended
-        // annotation performace drops by 10x if allocating all together
-        // when the array is created
-        if (values[coreId] == null)
+        // only the last index requires a cas, for the rest an ordered put is sufficient since only one thread will
+        // update the value.
+        long offset = counterOffset(coreId);
+        if (coreId < numCores)
         {
-            assert TPC.isValidCoreId(coreId);
-            values[coreId] = new Cell(0L);
+            // This will not work with concurrent reset
+            long value = UNSAFE.getLong(values, offset);
+            UNSAFE.putOrderedLong(values, offset, value + x);
         }
+        else
+        {
+            UNSAFE.getAndAddLong(values, offset, x);
+        }
+    }
 
-        // only the last index requires a cas, for the rest a lazy put
-        // is sufficient since only one thread will update this values
-        values[coreId].add(x, coreId < numCores);
+    private long counterOffset(int coreId)
+    {
+        return COUNTER_ARRAY_BASE + (coreId << COUNTER_OFFSET_SCALE);
     }
 
     public void increment() {
@@ -128,16 +87,18 @@ public class LongAdder
     public long sum()
     {
         long sum = 0;
-        for (Cell c : values)
-            if (c != null)
-                sum += c.get();
+        long[] values = this.values;
+        for (int i = 0; i <= numCores;i++)
+            sum += UNSAFE.getLongVolatile(values, counterOffset(i));
 
         return sum;
     }
 
     public void reset()
     {
-        Arrays.stream(values).filter(cell -> cell != null).forEach(cell -> cell.set(0L));
+        long[] values = this.values;
+        for (int i = 0; i <= numCores;i++)
+            UNSAFE.getAndSetLong(values, counterOffset(i), 0);
     }
 
     public String toString() {
