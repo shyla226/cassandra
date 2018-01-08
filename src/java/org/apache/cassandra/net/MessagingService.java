c4a15011ec5b749c2b944e4dd96616905622ac1d
Jake Luciani <jake@datastax.com>
2018-01-08 14:14:04 -0500

    Remove unused import

--- src/java/org/apache/cassandra/net/MessagingService.java
+++ src/java/org/apache/cassandra/net/MessagingService.java
@@ -80,7 +80,6 @@ import org.apache.cassandra.tracing.TraceState;
 import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.*;
 import org.apache.cassandra.utils.concurrent.SimpleCondition;
-import org.openjdk.jmh.util.HashsetMultimap;
 
 public final class MessagingService implements MessagingServiceMBean
 {
