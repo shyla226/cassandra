d035e19ce6d8e6dc0b1feb24d98b28c8d5757882
Robert Stupp <snazy@snazy.de>
2018-02-02 17:04:35 +0100

    DB-1656: Raise defaults for roles+permission caches (#704)

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -52,11 +52,11 @@ public class Config
     public String authenticator;
     public String authorizer;
     public String role_manager;
-    public volatile int permissions_validity_in_ms = 2000;
-    public volatile int permissions_cache_max_entries = 1000;
+    public volatile int permissions_validity_in_ms = 120_000;
+    public volatile int permissions_cache_max_entries = 1_000;
     public volatile int permissions_update_interval_in_ms = -1;
-    public volatile int roles_validity_in_ms = 2000;
-    public volatile int roles_cache_max_entries = 1000;
+    public volatile int roles_validity_in_ms = 120_000;
+    public volatile int roles_cache_max_entries = 1_000;
     public volatile int roles_update_interval_in_ms = -1;
     public boolean system_keyspaces_filtering;
 
