5c02e1937b3e141ff85053f02557303d5bacb1dc
Branimir Lambov <branimir.lambov@datastax.com>
2021-01-29 21:47:46 +0200

    Benchmarks

--- test/microbench/org/apache/cassandra/test/microbench/instance/WriteTest.java
+++ test/microbench/org/apache/cassandra/test/microbench/instance/WriteTest.java
@@ -32,8 +32,6 @@ import org.apache.cassandra.cql3.CQLTester;
 import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.commitlog.CommitLog;
-import org.apache.cassandra.db.memtable.Memtable;
-import org.apache.cassandra.utils.FBUtilities;
 import org.openjdk.jmh.annotations.*;
 
 @BenchmarkMode(Mode.AverageTime)
@@ -43,7 +41,7 @@ import org.openjdk.jmh.annotations.*;
 @Fork(value = 1)
 @Threads(1)
 @State(Scope.Benchmark)
-public abstract class ReadTest extends CQLTester
+public class WriteTest extends CQLTester
 {
     static String keyspace;
     String table;
@@ -53,16 +51,16 @@ public abstract class ReadTest extends CQLTester
     @Param({"1000"})
     int BATCH = 1_000;
 
-    public enum Flush
+    public enum EndOp
     {
-        INMEM, NO, YES
+        INMEM, TRUNCATE, FLUSH
     }
 
     @Param({"1000000"})
     int count = 1_000_000;
 
-    @Param({"INMEM", "YES"})
-    Flush flush = Flush.INMEM;
+    @Param({"INMEM", "TRUNCATE", "FLUSH"})
+    EndOp flush = EndOp.INMEM;
 
     @Param({""})
     String memtableClass = "";
@@ -78,98 +76,100 @@ public abstract class ReadTest extends CQLTester
     @Param({"PARALLEL"})
     Execution async = Execution.PARALLEL;
 
+    String writeStatement;
+
     @Setup(Level.Trial)
     public void setup() throws Throwable
     {
         rand = new Random(1);
         CQLTester.setUpClass();
         CQLTester.prepareServer();
+        DatabaseDescriptor.setAutoSnapshot(false);
         System.err.println("setupClass done.");
         String memtableSetup = "";
         if (!memtableClass.isEmpty())
             memtableSetup = String.format(" AND memtable = { 'class': '%s' }", memtableClass);
-        keyspace = createKeyspace("CREATE KEYSPACE %s with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 } and durable_writes = false");
-        table = createTable(keyspace, "CREATE TABLE %s ( userid bigint, picid bigint, commentid bigint, PRIMARY KEY(userid, picid)) with compression = {'enabled': false}" + memtableSetup);
-        execute("use "+keyspace+";");
+        keyspace = createKeyspace(
+        "CREATE KEYSPACE %s with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 } and durable_writes = false");
+        table = createTable(keyspace,
+                            "CREATE TABLE %s ( userid bigint, picid bigint, commentid bigint, PRIMARY KEY(userid, picid)) with compression = {'enabled': false}" +
+                            memtableSetup);
+        execute("use " + keyspace + ";");
         switch (async)
         {
-            case SERIAL_NET:
-            case PARALLEL_NET:
-                CQLTester.requireNetwork();
-                executeNet(getDefaultVersion(), "use " + keyspace + ";");
+        case SERIAL_NET:
+        case PARALLEL_NET:
+            CQLTester.requireNetwork();
+            executeNet(getDefaultVersion(), "use " + keyspace + ";");
         }
-        String writeStatement = "INSERT INTO "+table+"(userid,picid,commentid)VALUES(?,?,?)";
+        writeStatement = "INSERT INTO " + table + "(userid,picid,commentid)VALUES(?,?,?)";
         System.err.println("Prepared, batch " + BATCH + " flush " + flush);
-        System.err.println("Disk access mode " + DatabaseDescriptor.getDiskAccessMode() + " index " + DatabaseDescriptor.getIndexAccessMode());
+        System.err.println("Disk access mode " + DatabaseDescriptor.getDiskAccessMode() + " index " +
+                           DatabaseDescriptor.getIndexAccessMode());
 
         cfs = Keyspace.open(keyspace).getColumnFamilyStore(table);
         cfs.disableAutoCompaction();
         cfs.forceBlockingFlush();
+    }
 
-        //Warm up
-        long writeStart = System.currentTimeMillis();
-        System.err.println("Writing " + count);
+    @Benchmark
+    public void writeTable() throws Throwable
+    {
         long i;
         for (i = 0; i <= count - BATCH; i += BATCH)
-            performWrite(writeStatement, i, BATCH);
+            performWrite(i, BATCH);
         if (i < count)
-            performWrite(writeStatement, i, count - i);
-        long writeLength = System.currentTimeMillis() - writeStart;
-        System.err.format("... done in %.3f s.\n", writeLength / 1000.0);
-
-        Memtable memtable = cfs.getTracker().getView().getCurrentMemtable();
-        Memtable.MemoryUsage usage = memtable.getMemoryUsage();
-        System.err.format("%s in %s mode: %d ops, %s serialized bytes, %s\n",
-                          memtable.getClass().getSimpleName(),
-                          DatabaseDescriptor.getMemtableAllocationType(),
-                          memtable.getOperations(),
-                          FBUtilities.prettyPrintMemory(memtable.getLiveDataSize()),
-                          usage);
+            performWrite(i, count - i);
 
         switch (flush)
         {
-        case YES:
+        case FLUSH:
             cfs.forceBlockingFlush();
+            // if we flush we also must truncate to avoid accummulating sstables
+        case TRUNCATE:
+            execute("TRUNCATE TABLE " + table);
+            // note: we turn snapshotting and durable writes (which would have caused a flush) off for this benchmark
             break;
         case INMEM:
             if (!cfs.getLiveSSTables().isEmpty())
                 throw new AssertionError("SSTables created for INMEM test.");
+            // leave unflushed, i.e. next iteration will overwrite data
         default:
-            // don't flush
-        }
-
-        // Needed to stabilize sstable count for off-cache sized tests (e.g. count = 100_000_000)
-        while (cfs.getLiveSSTables().size() >= 15)
-        {
-            cfs.enableAutoCompaction(true);
-            cfs.disableAutoCompaction();
         }
     }
 
-    abstract Object[] writeArguments(long i);
+    public Object[] writeArguments(long i)
+    {
+        return new Object[] { i, i, i };
+    }
+//    abstract Object[] writeArguments(long i);
 
-    public void performWrite(String writeStatement, long ofs, long count) throws Throwable
+    public void performWrite(long ofs, long count) throws Throwable
     {
         switch (async)
         {
         case SERIAL:
+            performWriteSerial(ofs, count);
+            break;
         case SERIAL_NET:
-            performWriteSerial(writeStatement, ofs, count);
+            performWriteSerialNet(ofs, count);
             break;
         case PARALLEL:
+            performWriteThreads(ofs, count);
+            break;
         case PARALLEL_NET:
-            performWriteThreads(writeStatement, ofs, count);
+            performWriteThreadsNet(ofs, count);
             break;
         }
     }
 
-    public void performWriteSerial(String writeStatement, long ofs, long count) throws Throwable
+    public void performWriteSerial(long ofs, long count) throws Throwable
     {
         for (long i = ofs; i < ofs + count; ++i)
             execute(writeStatement, writeArguments(i));
     }
 
-    public void performWriteThreads(String writeStatement, long ofs, long count) throws Throwable
+    public void performWriteThreads(long ofs, long count) throws Throwable
     {
         long done = LongStream.range(ofs, ofs + count)
                               .parallel()
@@ -189,10 +189,36 @@ public abstract class ReadTest extends CQLTester
         assert done == count;
     }
 
+    public void performWriteSerialNet(long ofs, long count) throws Throwable
+    {
+        for (long i = ofs; i < ofs + count; ++i)
+            sessionNet().execute(writeStatement, writeArguments(i));
+    }
+
+    public void performWriteThreadsNet(long ofs, long count) throws Throwable
+    {
+        long done = LongStream.range(ofs, ofs + count)
+                              .parallel()
+                              .map(i ->
+                                   {
+                                       try
+                                       {
+                                           sessionNet().execute(writeStatement, writeArguments(i));
+                                           return 1;
+                                       }
+                                       catch (Throwable throwable)
+                                       {
+                                           throw Throwables.propagate(throwable);
+                                       }
+                                   })
+                              .sum();
+        assert done == count;
+    }
+
     @TearDown(Level.Trial)
     public void teardown() throws InterruptedException
     {
-        if (flush == Flush.INMEM && !cfs.getLiveSSTables().isEmpty())
+        if (flush == EndOp.INMEM && !cfs.getLiveSSTables().isEmpty())
             throw new AssertionError("SSTables created for INMEM test.");
 
         // do a flush to print sizes
