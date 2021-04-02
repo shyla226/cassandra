62316e4646c6139a7eb0f1ab3b270c9ac46c17fb
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2021-04-02 16:30:14 +0200

    STAR-247 fix NetstatsBootstrapWithEntireSSTablesCompressionStreamingTest

--- src/java/org/apache/cassandra/streaming/ProgressInfo.java
+++ src/java/org/apache/cassandra/streaming/ProgressInfo.java
@@ -109,7 +109,7 @@ public class ProgressInfo implements Serializable
         StringBuilder sb = new StringBuilder(fileName);
         sb.append(" ").append(currentBytes);
         sb.append("/").append(totalBytes).append(" bytes ");
-        sb.append("(").append(currentBytes*100/totalBytes).append("%) ");
+        sb.append("(").append(totalBytes == 0 ? 0 : currentBytes*100/totalBytes).append("%) ");
         sb.append(direction == Direction.OUT ? "sent to " : "received from ");
         sb.append("idx:").append(sessionIndex);
         sb.append(peer.toString(withPorts));
