a27abe013052a622abd519866db36485df5be9f6
jacek-lewandowski <jacek.lewandowski@datastax.com>
2021-04-07 18:18:22 +0200

    STAR-247: Refactor SSTable verfication

--- src/java/org/apache/cassandra/db/compaction/Verifier.java
+++ src/java/org/apache/cassandra/db/compaction/Verifier.java
@@ -140,7 +140,7 @@ public class Verifier implements Closeable
 
         try
         {
-            outputHandler.debug("Deserializing index for "+sstable);
+            outputHandler.debug("Deserializing index for " + sstable);
             deserializeIndex(sstable);
         }
         catch (Throwable t)
@@ -407,7 +407,10 @@ public class Verifier implements Closeable
     {
         try (PartitionIndexIterator it = sstable.allKeysIterator()) {
             //noinspection StatementWithEmptyBody
-            while (it.advance()); // no-op, just check if index is readable
+            ByteBuffer last = it.key();
+            while (it.advance()) last = it.key(); // no-op, just check if index is readable
+            if (!Objects.equals(last, sstable.last.getKey()))
+                throw new CorruptSSTableException(new IOException("Failed to read partition index"), it.toString());
         }
     }
 
