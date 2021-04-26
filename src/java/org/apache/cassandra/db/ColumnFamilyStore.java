c44aba9cf7460d7a08ab2ab6e61fb243bb5bdacf
Aleksandr Sorokoumov <918393+Gerrrr@users.noreply.github.com>
2021-04-26 17:04:21 +0200

    STAR-410 (#101)
    * STAR-410 Refactor compaction metrics
    
    Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    
    * STAR-410 Rename CompactionInfo.Holder to AbstractTableOperation and CompactionInfo to AbstractTableOperation.Progress
    
    CompactionInfo.Holder was implemented by all table operations, not just compactions. For example index and view building
    were reported as compactions. The new naming reflects more correctly the role of this class, which is to report progress or
    interrupt these table operations.
    
    CompactionInfo was a basic class capable of reporting the progress of an operation, for example how many bytes (or keys, or ranges)
    have been currently processed out of a total number of bytes (or keys, or ranges).
    
    The new names should remove the previous confusion, and allow adding further compaction progress statistics.
    
    Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    
    * STAR-410 More refactoring of compaction-related code:
    
    - Added CompactionProgress and refactored CompactionTask into CompactionOperation
    - Extracted interfaces from AbstractTableOperation and its progress
      inner-class, so that they can be extended by CompactionProgress
    - Added compactions in progress to the abstract strategy for a basic
      aggregation of compactions in progress, to be extended further
    
    Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    
    * STAR-410 New aggregated statistics for compaction strategies added to nodetool compactionstats and compaction logger
    
     Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
     Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    
    * STAR-410 Rework compaction strategy code to remove repetition and clarify
    
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
    
    * STAR-410 Minor refactoring of compaction strategies and fixing of CI failures:
    
    - Refactor getEstimatedRemainingTasks() and getMaximalTask for STCS, nits
    - Fix a possible deadlock in CompactionStrategyManager
    - Fix strong self-ref loop
      A strong self-ref loop of SSTableReader was caused by background compactions  because the
      compaction strategy manager is an observer of the tracker. So long as the txn sstable tidier
      references the tracker, then the tracker references the csm and the sstables via the background
      compaction aggregates or picks.
    
    Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    
    * STAR-410 fix compaction interruptions, refactor compaction iterator to return a separate table operation
    
    Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    
    * STAR-410 Undo some changes in ActiveOperations
    
    * Store all active operations in the same set instead of a by-table map
    * ActiveOperations does not extend SchemaChangeListener
    
    * fixup! STAR-410 Minor refactoring of compaction strategies and fixing of CI failures:
    
    * Review: Use throw Throwables.propagate instead of hrowables.maybeFail when catching exceptions
    
    * Review: fix wording in BackgroundCompactions and CompactionTask
    
    * Review: Remove unnecessary maybeReloadDiskBoundaries() call
    
    * STAR-410 Compaction strategies filter out SSTables that are no longer in the live set
    
    Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -403,6 +403,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
 
         // compaction strategy should be created after the CFS has been prepared
         compactionStrategyManager = new CompactionStrategyManager(this);
+        compactionStrategyManager.reload(metadata().params.compaction);
 
         if (maxCompactionThreshold.value() <= 0 || minCompactionThreshold.value() <=0)
         {
@@ -815,6 +816,11 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
     public String getTableName()
     {
         return name;
+    }
+
+    public String getKeyspaceName()
+    {
+        return keyspace.getName();
     }
 
     public Descriptor newSSTableDescriptor(File directory)
@@ -1587,9 +1593,19 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
         return data.getView().select(sstableSet);
     }
 
-    public Iterable<SSTableReader> getUncompactingSSTables()
+    public Iterable<SSTableReader> getNoncompactingSSTables()
     {
-        return data.getUncompacting();
+        return data.getNoncompacting();
+    }
+
+    public Iterable<? extends SSTableReader> getNoncompactingSSTables(Iterable<? extends SSTableReader> candidates)
+    {
+        return data.getNoncompacting(candidates);
+    }
+
+    public Set<SSTableReader> getCompactingSSTables()
+    {
+        return data.getCompacting();
     }
 
     public Map<UUID, PendingStat> getPendingRepairStats()
@@ -2296,6 +2312,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
         {
             cfs.runWithCompactionsDisabled((Callable<Void>) () -> {
                 cfs.data.reset(memtableFactory.create(new AtomicReference<>(CommitLogPosition.NONE), cfs.metadata, cfs));
+                cfs.compactionStrategyManager.forceReload();
                 return null;
             }, true, false);
         }
@@ -2382,7 +2399,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
             }
         };
 
-        runWithCompactionsDisabled(Executors.callable(truncateRunnable), true, true, CompactionInfo.StopTrigger.TRUNCATE);
+        runWithCompactionsDisabled(Executors.callable(truncateRunnable), true, true, AbstractTableOperation.StopTrigger.TRUNCATE);
 
         viewManager.build();
         logger.info("Truncate of {}.{} is complete", keyspace.getName(), name);
@@ -2415,14 +2432,14 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
         return runWithCompactionsDisabled(callable, (sstable) -> true, interruptValidation, interruptViews, true);
     }
 
-    public <V> V runWithCompactionsDisabled(Callable<V> callable, boolean interruptValidation, boolean interruptViews, CompactionInfo.StopTrigger trigger)
+    public <V> V runWithCompactionsDisabled(Callable<V> callable, boolean interruptValidation, boolean interruptViews, AbstractTableOperation.StopTrigger trigger)
     {
         return runWithCompactionsDisabled(callable, (sstable) -> true, interruptValidation, interruptViews, true, trigger);
     }
 
     public <V> V runWithCompactionsDisabled(Callable<V> callable, Predicate<SSTableReader> sstablesPredicate, boolean interruptValidation, boolean interruptViews, boolean interruptIndexes)
     {
-        return runWithCompactionsDisabled(callable, sstablesPredicate, interruptValidation, interruptViews, interruptIndexes, CompactionInfo.StopTrigger.NONE);
+        return runWithCompactionsDisabled(callable, sstablesPredicate, interruptValidation, interruptViews, interruptIndexes, AbstractTableOperation.StopTrigger.NONE);
     }
 
     /**
@@ -2435,7 +2452,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
      * @param interruptIndexes if we should interrupt compactions on indexes. NOTE: if you set this to true your sstablePredicate
      *                         must be able to handle LocalPartitioner sstables!
      */
-    public <V> V runWithCompactionsDisabled(Callable<V> callable, Predicate<SSTableReader> sstablesPredicate, boolean interruptValidation, boolean interruptViews, boolean interruptIndexes, CompactionInfo.StopTrigger trigger)
+    public <V> V runWithCompactionsDisabled(Callable<V> callable, Predicate<SSTableReader> sstablesPredicate, boolean interruptValidation, boolean interruptViews, boolean interruptIndexes, AbstractTableOperation.StopTrigger trigger)
     {
         // synchronize so that concurrent invocations don't re-enable compactions partway through unexpectedly,
         // and so we only run one major compaction at a time
@@ -2583,6 +2600,11 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
     public CompactionStrategyManager getCompactionStrategyManager()
     {
         return compactionStrategyManager;
+    }
+
+    public CompactionLogger getCompactionLogger()
+    {
+        return compactionStrategyManager == null ? null : compactionStrategyManager.compactionLogger;
     }
 
     public void setCrcCheckChance(double crcCheckChance)
